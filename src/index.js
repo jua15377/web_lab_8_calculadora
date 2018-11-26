b7cd38adec756815bc1a35497ef56ef6a0f6870b
jua15377 <jua15377@uvg.edu.gt>
2018-11-25 20:11:22 -0600

    set linter

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './style.css'
 
-class App extends React.Component{
-	render(){
-		return(
-			<h1>HelloW</h1>
-		)
-	}
-	
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return (<h1>HelloW</h1>)
+  }
 }
 
-ReactDOM.render( <App/>, document.getElementById('index'))
+ReactDOM.render(<App />, document.getElementById('index'))
